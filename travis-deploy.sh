c7ba3ef60f40bb61b553867a2d89a15237dc7d4a
Damian Mee <spam@meedamian.com>
2019-02-08 22:20:45 +0700

    arch -> goarch fix

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -3,33 +3,30 @@ set -e
 
 image="lncm/lnd"
 
-docker tag lnd "$image:$TRAVIS_TAG-linux-arm"
-docker push "$image:$TRAVIS_TAG-linux-arm"
+docker tag lnd "$image:${TRAVIS_TAG}-linux-arm"
+docker push "$image:${TRAVIS_TAG}-linux-arm"
 
 set +e
-# this will probably never be necessary, as docker hub is fast and travis+qemu are **really** slow
-echo "Waiting for docker hub to finish building $image:$TRAVIS_TAG-linux-amd64"
-
-if [[ "$(docker images -q "$image:$TRAVIS_TAG-linux-amd64" 2> /dev/null)" == "" ]]; then
+if [[ "$(docker images -q "$image:${TRAVIS_TAG}-linux-amd64" 2> /dev/null)" == "" ]]; then
     sleep 15
-    echo "waiting for $image:$TRAVIS_TAG-linux-amd64 to finish building…"
+    echo "waiting for $image:${TRAVIS_TAG}-linux-amd64 to finish building…"
 fi
 set -e
 
-echo "Pushing manifest $image:$TRAVIS_TAG"
-docker -D manifest create "$image:$TRAVIS_TAG" \
-    "$image:$TRAVIS_TAG-linux-amd64" \
-    "$image:$TRAVIS_TAG-linux-arm"
+echo "Pushing manifest $image:${TRAVIS_TAG}"
+docker -D manifest create "$image:${TRAVIS_TAG}" \
+    "$image:${TRAVIS_TAG}-linux-amd64" \
+    "$image:${TRAVIS_TAG}-linux-arm"
 
-docker manifest annotate "$image:$TRAVIS_TAG" "$image:$TRAVIS_TAG-linux-arm" --os linux --arch arm --variant v6
-docker manifest push "$image:$TRAVIS_TAG"
+docker manifest annotate "$image:${TRAVIS_TAG}" "$image:${TRAVIS_TAG}-linux-arm" --os linux --arch arm --variant v6
+docker manifest push "$image:${TRAVIS_TAG}"
 
 
 echo "Pushing manifest $image:latest"
 docker -D manifest create "$image:latest" \
-    "$image:$TRAVIS_TAG-linux-amd64" \
-    "$image:$TRAVIS_TAG-linux-arm"
+    "$image:${TRAVIS_TAG}-linux-amd64" \
+    "$image:${TRAVIS_TAG}-linux-arm"
 
-docker manifest annotate "$image:latest" "$image:$TRAVIS_TAG-linux-arm" --os linux --arch arm --variant v6
+docker manifest annotate "$image:latest" "$image:${TRAVIS_TAG}-linux-arm" --os linux --arch arm --variant v6
 docker manifest push "$image:latest"
 
