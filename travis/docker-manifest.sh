e9652a95a00761546e7a95ee979468190c183c14
BT <nolim1t@users.noreply.github.com>
2019-09-12 18:22:32 +0700

    Add ARM8 to manifest building script

--- travis/docker-manifest.sh
+++ travis/docker-manifest.sh
@@ -20,16 +20,18 @@ IMAGE_VERSIONED="${SLUG}:${TRAVIS_TAG}"
 IMAGE_AMD64="${IMAGE_VERSIONED}-linux-amd64"
 IMAGE_ARM6="${IMAGE_VERSIONED}-linux-armv6"
 IMAGE_ARM7="${IMAGE_VERSIONED}-linux-armv7"
+IMAGE_ARM64="${IMAGE_VERSIONED}-linux-armv8"
 
 docker pull "${IMAGE_AMD64}"
 docker pull "${IMAGE_ARM6}"
 docker pull "${IMAGE_ARM7}"
-
+docker pull "${IMAGE_ARM64}"
 
 echo     "Pushing manifest ${IMAGE_VERSIONED}"
 docker -D manifest create "${IMAGE_VERSIONED}"  "${IMAGE_AMD64}"  "${IMAGE_ARM6}"  "${IMAGE_ARM7}"
 docker manifest annotate  "${IMAGE_VERSIONED}"  "${IMAGE_ARM6}"  --os linux  --arch arm  --variant v6
 docker manifest annotate  "${IMAGE_VERSIONED}"  "${IMAGE_ARM7}"  --os linux  --arch arm  --variant v7
+docker manifest annotate "${IMAGE_VERSIONED}" "${IMAGE_ARM64}" --os linux --arch arm64 --variant v8
 docker manifest push      "${IMAGE_VERSIONED}"
 
 
@@ -40,6 +42,7 @@ echo     "Pushing manifest ${IMAGE_MINOR_VER}"
 docker -D manifest create "${IMAGE_MINOR_VER}"  "${IMAGE_AMD64}"  "${IMAGE_ARM6}"  "${IMAGE_ARM7}"
 docker manifest annotate  "${IMAGE_MINOR_VER}"  "${IMAGE_ARM6}"  --os linux  --arch arm  --variant v6
 docker manifest annotate  "${IMAGE_MINOR_VER}"  "${IMAGE_ARM7}"  --os linux  --arch arm  --variant v7
+docker manifest annotate "${IMAGE_MINOR_VER}" "${IMAGE_ARM64}" --os linux --arch arm64 --variant v8
 docker manifest push      "${IMAGE_MINOR_VER}"
 
 
@@ -50,4 +53,5 @@ echo     "Pushing manifest ${IMAGE_LATEST}"
 docker -D manifest create "${IMAGE_LATEST}"  "${IMAGE_AMD64}"  "${IMAGE_ARM6}"  "${IMAGE_ARM7}"
 docker manifest annotate  "${IMAGE_LATEST}"  "${IMAGE_ARM6}"  --os linux  --arch arm  --variant v6
 docker manifest annotate  "${IMAGE_LATEST}"  "${IMAGE_ARM7}"  --os linux  --arch arm  --variant v7
+docker manifest annotate "${IMAGE_LATEST}" "${IMAGE_ARM64}" --os linux --arch arm64 --variant v8
 docker manifest push      "${IMAGE_LATEST}"
