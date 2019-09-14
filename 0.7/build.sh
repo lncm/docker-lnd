43fee1d4690c1320106598773b1fb438c1f3507c
Damian Mee <spam@meedamian.com>
2019-09-15 06:00:40 +0700

    Github Actions vol 2

--- 0.7/build.sh
+++ 0.7/build.sh
@@ -6,25 +6,17 @@ ARCH=$1
 
 PKG="github.com/lightningnetwork/lnd"
 COMMIT="$(git describe --abbrev=40 --dirty)"
-COMMITFLAGS="-X ${PKG}/build.Commit=${COMMIT}"
+COMMITFLAGS="-buildid= -X ${PKG}/build.Commit=${COMMIT}"
 
 export GOOS=linux
-export GOARCH="${ARCH}"
 
 case "${ARCH}" in
 arm32v6) export GOARCH=arm GOARM=6 ;;
 arm32v7) export GOARCH=arm GOARM=7 ;;
+*)       export GOARCH="${ARCH}"   ;;
 esac
 
-# NOTE: needed because of https://github.com/lightningnetwork/lnd/issues/3506
-BTCWALLET=github.com/btcsuite/btcwallet
-go mod edit -replace="${BTCWALLET}=${BTCWALLET}@v0.0.0-20190814023431-505acf51507f"
+TAGS="autopilotrpc invoicesrpc walletrpc routerrpc watchtowerrpc neutrino"
 
-TAGS="autopilotrpc invoicesrpc walletrpc routerrpc watchtowerrpc"
-
-mkdir -p /bin/
-
-go build -v -trimpath -ldflags "${COMMITFLAGS}" -tags="${TAGS} signrpc chainrpc" -o /bin/ "${PKG}/cmd/lnd"
-go build -v -trimpath -ldflags "${COMMITFLAGS}" -tags="${TAGS}"                  -o /bin/ "${PKG}/cmd/lncli"
-
-ls -la /bin/
+go build -v -trimpath -mod=readonly -o /go/bin/ -ldflags "${COMMITFLAGS}" -tags="${TAGS} signrpc chainrpc" "${PKG}/cmd/lnd"
+go build -v -trimpath -mod=readonly -o /go/bin/ -ldflags "${COMMITFLAGS}" -tags="${TAGS}"                  "${PKG}/cmd/lncli"
