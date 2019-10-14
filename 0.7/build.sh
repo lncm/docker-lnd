4436bfbba0d6a95074e33094840b9f898059f2fd
Damian Mee <spam@meedamian.com>
2019-10-14 05:43:15 +0200

    Make binaries reproducible again!

--- 0.7/build.sh
+++ 0.7/build.sh
@@ -31,6 +31,11 @@ TAGS="autopilotrpc invoicesrpc walletrpc routerrpc watchtowerrpc"
 # Added by yours truly (@lncm)
 TAGS="${TAGS} wtclientrpc"
 
+# Added to make output binary static
+#   ctx: https://github.com/golang/go/issues/26492
+# TODO: Check if still needed after Go v1.14 release
+TAGS="${TAGS} osusergo netgo static_build"
+
 build() {
   binary_name=$1
   extra_tags=$2
