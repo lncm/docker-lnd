9a517e2c18f8261de0ddf607517aa059c704dd13
Damian Mee <spam@meedamian.com>
2020-08-06 21:23:52 +0800

    list tags script

--- scripts/list-tags.sh
+++ scripts/list-tags.sh
@@ -11,12 +11,11 @@ main() {
 
   curl -s "https://registry.hub.docker.com/v1/repositories/$repo/tags" \
     | jq -r '.[].name' \
-    | grep '^v.*' \
-    | grep -v '\-build' \
-    | tr -s '-' '~' \
+    | grep ^v | grep -v '\-build' \
+    | tr -s - \~ \
     | sort -Vr | uniq \
-    | tr -s '~' '-' \
-    | grep -v '\-\(arm32\|arm64\|amd64\|linux-arm\)' \
+    | tr \~ - \
+    | grep -Ev '\-(arm(32|64)|amd64|linux-arm)' \
     | awk -F- 'NR==1 {printf "* "}; $1!=a && NR>1 {print "\n* "}; {ORS=""; printf "`%s` ", $0}; {a=$1}'
 
     echo
